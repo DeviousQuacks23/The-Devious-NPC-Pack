16f0134428e34f77f6a5e43cec494eb712f38039
deviousquacks23 <duchluong1603@gmail.com>
2025-10-08 12:10:10 +0800

    Update blarggs.lua

--- 1.3 NPC Recreations/Blargg/blarggs.lua
+++ 1.3 NPC Recreations/Blargg/blarggs.lua
@@ -99,7 +99,7 @@ function blarggs.onTickNPC(v)
 		if v.y > (v.spawnY + v.height + config.leapDespawnOffset) then
 			data.scale = math.max(0, data.scale - config.shrinkSpeed)
 			if data.scale <= 0 then
-				v:mem(0x124,FIELD_BOOL, false)
+				v:mem(0x124,FIELD_BOOL, true)
 				v.despawnTimer = 0
 			end
 		end
