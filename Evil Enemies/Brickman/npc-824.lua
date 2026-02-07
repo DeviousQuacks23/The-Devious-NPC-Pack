e82190962f96b1156264bb2d6562d92137dc630a
deviousquacks23 <duchluong1603@gmail.com>
2026-02-07 15:35:31 +0800

    Revamped Brickmans

--- Evil Enemies/Brickman/npc-824.lua
+++ Evil Enemies/Brickman/npc-824.lua
@@ -9,13 +9,12 @@ local brickmanSettings = {
 
 	gfxwidth = 128,
 	gfxheight = 64,
+	gfxoffsety = 2,
 	width = 64,
 	height = 48,
 	frames = 3,
 	framestyle = 0,
 	framespeed = 12, 
-
-	speed = 0,
 
 	nofireball = true,
 	noiceball = true,
@@ -23,18 +22,19 @@ local brickmanSettings = {
 
 	score = 2,
 	weight = 2,
+	isstationary = true,
 
         delay = 120,
         brickInterval = 5,
         volleyMin = 3,
         volleyMax = 10,
         brickSpawn = 825,
+
+	deathEffectID = 51,
+	deathSFX = 4,
 }
 
 npcManager.setNpcSettings(brickmanSettings)
-
-local deathEffectID = (51)
-
 npcManager.registerHarmTypes(npcID,
 	{
 		HARM_TYPE_JUMP,
@@ -49,29 +49,22 @@ npcManager.registerHarmTypes(npcID,
 		HARM_TYPE_SWORD
 	}, 
 	{
-		[HARM_TYPE_JUMP]=deathEffectID,
-		[HARM_TYPE_FROMBELOW]=deathEffectID,
-		[HARM_TYPE_NPC]=deathEffectID,
-		[HARM_TYPE_PROJECTILE_USED]=deathEffectID,
 		[HARM_TYPE_LAVA]={id=13, xoffset=0.5, xoffsetBack = 0, yoffset=1, yoffsetBack = 1.5},
-		[HARM_TYPE_HELD]=deathEffectID,
-		[HARM_TYPE_TAIL]=deathEffectID,
-		[HARM_TYPE_SPINJUMP]=10,
 		[HARM_TYPE_OFFSCREEN]=10,
-		[HARM_TYPE_SWORD]=deathEffectID,
 	}
 );
 
 function brickman.onInitAPI()
 	npcManager.registerEvent(npcID, brickman, "onTickEndNPC")
         npcManager.registerEvent(npcID, brickman, "onDrawNPC")
+	registerEvent(brickman, "onPostNPCKill")
 end
 
 function brickman.onTickEndNPC(v)
 	if Defines.levelFreeze then return end
 	
 	local data = v.data
-        local cfg = NPC.config[npcID]
+        local cfg = NPC.config[v.id]
 	
 	if v.despawnTimer <= 0 then
 		data.initialized = false
@@ -80,7 +73,7 @@ function brickman.onTickEndNPC(v)
 
 	if not data.initialized then
 		data.initialized = true
-                data.animationTimer = 0
+                data.animationTimer = 1
                 data.brickTimer = 0
                 data.shakeTimer = 0
                 data.offset = 0
@@ -88,23 +81,17 @@ function brickman.onTickEndNPC(v)
                 data.isDroppingBricks = false
 	end
 
-	if v.heldIndex ~= 0
-	or v.isProjectile  
-	or v.forcedState > 0
-	then
-	     return
-	end
-
         if data.isDroppingBricks then
         	data.animationTimer = data.animationTimer + 1
         	data.shakeTimer = data.shakeTimer + 1
-		v.animationFrame = math.min(cfg.frames-1,math.floor((data.animationTimer)/cfg.framespeed))
         else
-		v.animationFrame = 0
-        	data.animationTimer = 0
+        	data.animationTimer = data.animationTimer - 1
         	data.shakeTimer = 0
         	data.offset = 0
         end
+
+	v.animationFrame = math.floor(data.animationTimer / cfg.framespeed) % cfg.frames
+	data.animationTimer = math.clamp(data.animationTimer, 1, (cfg.frames * cfg.framespeed) - 2)
 
         if data.shakeTimer ~= 0 then
         	if data.shakeTimer%4 > 0 and data.shakeTimer%4 < 3 then
@@ -113,6 +100,13 @@ function brickman.onTickEndNPC(v)
                		data.offset = data.offset + 2
                	end
         end
+
+	if v.heldIndex ~= 0
+	or v.isProjectile  
+	or v.forcedState > 0
+	then
+	     return
+	end
 
         data.brickTimer = data.brickTimer + 1
 
@@ -139,17 +133,53 @@ function brickman.onTickEndNPC(v)
 end
 
 function brickman.onDrawNPC(v)
-    	if v:mem(0x12A, FIELD_WORD) <= 0 then
+    	if v:mem(0x12A, FIELD_WORD) <= 0 or v.isHidden then
         	return
     	end
 
-    	if not v.data.offset then return end
+    	if not v.data.offset or v.data.offset == 0 then return end
 
     	npcutils.drawNPC(v, {
         	xOffset = v.data.offset
     	})
 
     	npcutils.hideNPC(v)
+end
+
+-- Taken from MDA's Bullet Bills
+local function spawnEffects(effectID,x,y,width,height)
+        local effectConfig = Effect.config[effectID][1]
+    
+        local effectWidth = effectConfig.width
+        local effectHeight = effectConfig.height
+    
+        local effectCountX = math.floor(width /effectWidth  + 0.5)
+        local effectCountY = math.floor(height/effectHeight + 0.5)
+    
+	for idxX = 1,effectCountX do
+            	for idxY = 1,effectCountY do
+                	local effectX = x - effectCountX*effectWidth *0.5 + (idxX - 1)*effectWidth
+                	local effectY = y - effectCountY*effectHeight*0.5 + (idxY - 1)*effectHeight
+    
+                	Effect.spawn(effectID,effectX,effectY)
+            	end
+        end
+end
+
+function brickman.onPostNPCKill(v,reason)
+	if v.id ~= npcID then
+		return
+	end
+
+	if reason ~= HARM_TYPE_OFFSCREEN and reason ~= HARM_TYPE_LAVA then
+		local config = NPC.config[v.id]
+
+		spawnEffects(131,v.x + v.width*0.5,v.y + v.height*0.5,v.width,v.height)
+		if config.deathSFX then SFX.play(config.deathSFX) end
+		if config.deathEffectID and config.deathEffectID > 0 then
+			spawnEffects(config.deathEffectID,v.x + v.width*0.5,v.y + v.height*0.5,v.width,v.height)
+		end
+	end
 end
 
 return brickman
